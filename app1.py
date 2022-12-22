f7ad5a817e59d134879d8f081481ad2db6c42cc7
sky-forest <120348588+sky-forest@users.noreply.github.com>
2022-12-22 12:52:43 +0900

    add

--- app1.py
+++ app1.py
@@ -7,5 +7,6 @@ import altair as alt
 def app_1():
     st.subheader('반갑습니다.')
     st.text('해당 자료는 1983 ~ 2012 출시된 비디오게임의 데이터로 이루어졌습니다.')
+    st.text('원하는 게임을 검색하거나, 두 게임을 비교분석 할 수있습니다.')
 
-    st.image('https://img.redbull.com/images/c_fill,g_auto,w_400,h_540/q_auto:low,f_auto/redbullcom/2021/10/15/b0ithd7jhki2ifoak1ae/dd')
+    st.image('https://storage.googleapis.com/kaggle-datasets-images/2699607/4643723/850b03586df0489cd97b7c1d5cd93258/dataset-cover.png?t=2022-12-03-11-22-57')
--- app2.py
+++ app2.py
@@ -5,6 +5,5 @@ import plotly.express as px
 import altair as alt
 
 def app_2():
-    df= pd.read_csv('Video_Games_Sales_new.csv', index_col= 0)
+    df= pd.read_csv('Video_Games_Sales_new.csv', index_col= 1)
     st.dataframe(df)
-    st.dataframe(df.describe())
