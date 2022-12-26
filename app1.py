29c1bd7bf26db050445108ebbfe220e68eb892d1
sky-forest <120348588+sky-forest@users.noreply.github.com>
2022-12-26 11:14:25 +0900

    fix

--- app1.py
+++ app1.py
@@ -3,7 +3,7 @@ import streamlit as st
 def app_1():
     st.subheader('반갑습니다.')
     st.text('해당 자료는 1983년 부터 2012년 까지 출시된 비디오게임의 정보들입니다.')
-    st.text('게임을 검색하거나, 게임 정보를 확인 및 비교 할 수있습니다.')
+    st.text('게임을 검색하고, 정보를 확인 할 수있습니다.')
 
     st.image('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFRhKo1StjpdtSIg0MheVyZWgq9mF1NNubNQ&usqp=CAU')
     st.image('http://cdn.gameple.co.kr/news/photo/202003/152649_157010_2449.jpg')
